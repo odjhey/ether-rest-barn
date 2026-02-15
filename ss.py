c23632454526ac66d3bbef91b3346ebdf6b19995
odjhey <odjhey@gmail.com>
2026-02-15 11:35:27 +0800

    add grid and other maths

--- ss.py
+++ ss.py
@@ -21,22 +21,44 @@ def activate_win():
 
 activate_win()
 
+Offset = collections.namedtuple("Offset", 
+                                ["from_right", "from_top","from_left"],
+                                defaults=[0,0,0])
 def get_module_stat_subscreen():
     top = win.top
     right = win.right
-    Offset = collections.namedtuple("Offset", ["from_right", "from_top"])
-    offset = Offset(503, 135)
+    offset = Offset(from_right=503, from_top=135)
     subscr_width = 400
     subscr_height = 440
     return top+offset.from_top, right-offset.from_right, subscr_width, subscr_height
 
+def get_module_grid_subscreen():
+    top = win.top
+    left = win.left
+    offset = Offset(from_left=165, from_top=132)
+    subscr_width = 855
+    subscr_height = 712
+    return top+offset.from_top, left+offset.from_left, subscr_width, subscr_height
 
-with mss.mss() as sct:
-    top, left, width, height = get_module_stat_subscreen()
-    region = {'top': top, 'left': left, 'width': width, 'height': height}
-    sct_img = sct.grab(region)
-    mss.tools.to_png(sct_img.rgb, sct_img.size, output='shot.png')
+def get_module_center_by_idx(idx, grid_cols=7):
+    """get x,y of the center of a module by idx in grid, idx starts at 0"""
+    top, left, _, _ = get_module_grid_subscreen()
+    subscr_width = 139
+    subscr_height = subscr_width
+    # x,y in grid
+    grid_x,grid_y= idx%grid_cols,idx//grid_cols
+    subscr_top, subscr_left =  top+(grid_y*subscr_height), left+(grid_x*subscr_width)
+    center_top, center_left = subscr_top+(subscr_height//2), subscr_left+(subscr_width//2)
 
-#pyautogui.click(500, 500)
-#pyautogui.dragTo(600, 600, duration=0.4)
-#pyautogui.scroll(-200)
+    return center_left, center_top
+
+for idx in range(20):
+    time.sleep(0.200)
+    x,y = get_module_center_by_idx(idx)
+    pyautogui.click(x, y)
+    with mss.mss() as sct:
+        top, left, width, height = get_module_stat_subscreen()
+        region = {'top': top, 'left': left, 'width': width, 'height': height}
+        sct_img = sct.grab(region)
+        mss.tools.to_png(sct_img.rgb, sct_img.size, output=f"outs/xshot_{idx}.png")
+
